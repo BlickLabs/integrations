68ec1c72fef8af9a8b0e5aae608cb9b45a1f8e0c
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-03-15 10:37:47 -0600

    #1, Update production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,6 @@ Production settings
 """
 
 from .base import *
-import dj_database_url
 
 # DEBUG
 # -----------------------------------------------------------------------------
@@ -16,8 +15,6 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
 SECRET_KEY = env("DJANGO_SECRET_KEY", default='CHANGEME!!!')
 
-# This ensures that Django will be able to detect a secure connection
-# properly on Heroku.
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # SITE CONFIGURATION
@@ -26,19 +23,6 @@ ALLOWED_HOSTS = ["*"]
 
 # STATIC FILE CONFIGURATION
 # -----------------------------------------------------------------------------
-INSTALLED_APPS += ('storages', )
-
-STATIC_URL = env("AWS_BUCKET_URL")
-
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-
-AWS_S3_HOST = env("AWS_S3_HOST")
 
 # MEDIA CONFIGURATION
 # -----------------------------------------------------------------------------
