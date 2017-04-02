782c4f426420e8a3c33bbd67e71726faead3d2de
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-04-02 15:42:29 -0500

    #1, Add mailchimp views

---
+++ integrations/apps/mailchimp/urls.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+
+    url(regex='^finacero/newsletter/$',
+        view=views.FinaceroNewsletterView.as_view(),
+        name='finacero_newsletter'),
+
+    url(regex='^workinglabs/newsletter/$',
+        view=views.WorkingLabsNewsletterView.as_view(),
+        name='workinglabs_newsletter'),
+
+    url(regex='^rochalanderos/newsletter/$',
+        view=views.RochaLanderosNewsletterView.as_view(),
+        name='rochalanderos_newsletter'),
+
+]
---
+++ integrations/apps/mailchimp/views.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+from integrations.core.views import MailchimpGenericNewsletterView
+
+
+class FinaceroNewsletterView(MailchimpGenericNewsletterView):
+    KEY = settings.FINACERO_MAILCHIMP_API_KEY
+    LIST_ID = settings.FINACERO_MAILCHIMP_LIST_ID
+    SHARD = settings.FINACERO_MAILCHIMP_SHARD
+
+
+class WorkingLabsNewsletterView(MailchimpGenericNewsletterView):
+    KEY = settings.WORKING_LABS_MAILCHIMP_API_KEY
+    LIST_ID = settings.WORKING_LABS_MAILCHIMP_LIST_ID
+    SHARD = settings.WORKING_LABS_MAILCHIMP_SHARD
+
+class RochaLanderosNewsletterView(MailchimpGenericNewsletterView):
+    KEY = settings.ROCHA_LANDEROS_MAILCHIMP_API_KEY
+    LIST_ID = settings.ROCHA_LANDEROS_MAILCHIMP_LIST_ID
+    SHARD = settings.ROCHA_LANDEROS_MAILCHIMP_SHARD
